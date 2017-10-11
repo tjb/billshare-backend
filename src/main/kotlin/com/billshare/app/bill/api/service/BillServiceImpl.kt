534f1565c47e5d559644f5f8b0c612cf7eaf8fb0
Derek Gliwa <dgliwa7bhs@gmail.com>
2017-10-11 08:14:22 -0700

    #2 use jpa repository instead of session factory.

--- src/main/kotlin/com/billshare/app/bill/api/service/BillServiceImpl.kt
+++ src/main/kotlin/com/billshare/app/bill/api/service/BillServiceImpl.kt
@@ -1,29 +1,18 @@
 package com.billshare.app.bill.api.service
 
-import com.billshare.app.access.user.domain.User
 import com.billshare.app.bill.domain.Bill
 import com.billshare.app.bill.domain.BillRepository
 import com.billshare.app.bill.domain.UserBill
-import org.hibernate.SessionFactory
+import com.billshare.app.bill.domain.UserBillRepository
 import org.springframework.stereotype.Service
-import javax.transaction.Transactional
 
 @Service
-class BillServiceImpl(val billRepository: BillRepository, val sessionFactory: SessionFactory): BillService {
+class BillServiceImpl(val billRepository: BillRepository, val userBillRepository: UserBillRepository): BillService {
 
-    @Transactional
     override fun addUser(billId: Long, userId: Long, userBill: UserBill) {
-        val currentSession = sessionFactory.currentSession
-        val bill = currentSession.load(Bill::class.javaObjectType, billId)
-        val user = currentSession.load(User::class.javaObjectType, userId)
-        userBill.user = user
-        userBill.bill = bill
-        userBill.userBillId?.userId = user.id
-        userBill.userBillId?.billId = bill.id
-        bill.users = bill.users.plus(userBill)
-        currentSession.transaction.begin()
-        billRepository.save(bill)
-        currentSession.transaction.commit()
+        userBill.userBillId?.userId = userId
+        userBill.userBillId?.billId = billId
+        userBillRepository.save(userBill)
     }
 
     override fun save(bill: Bill): Bill {
